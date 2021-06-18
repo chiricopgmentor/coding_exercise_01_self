e41261de9e1f0740742aeb31f3305ff755a59562
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-19 06:41:03 +0900

    ハンバーガーメニュー実装

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // ハンバーガーメニュー動作
 $('.hamburger_btn').click(function () {
   $('.hamburger_btn__bar').toggleClass('hamburger_btn__bar--cross');
+  $('.header_nav').toggleClass('header_nav--menu_open');
+  $('body').toggleClass('noscroll');
+});
+// ハンバーガーメニュークリック時の動作
+$('.in_site_link').click(function () {
+  $('.hamburger_btn__bar').removeClass('hamburger_btn__bar--cross');
+  $('.header_nav').removeClass('header_nav--menu_open');
+  $('body').removeClass('noscroll');
 });
