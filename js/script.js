2e7146d787f156939843c181f81cbee01e7f0b74
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-19 12:47:43 +0900

    ハンバーガーメニューの微調整

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ $('.in_site_link').click(function () {
   $('.header_nav').removeClass('header_nav--menu_open');
   $('body').removeClass('noscroll');
 });
+
+// 画面リサイズ時の処理
+$(window).resize(function () {
+  const window_width = $(window).width();
+  if (window_width > 768) {
+    // PC版サイズ
+    $('.hamburger_btn__bar').removeClass('hamburger_btn__bar--cross');
+    $('.header_nav').removeClass('header_nav--menu_open');
+    $('body').removeClass('noscroll');
+  }
+});
